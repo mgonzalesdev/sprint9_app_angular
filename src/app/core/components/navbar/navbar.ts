5421dc18daee8a9a086aa2e8184520c9b617b4d2
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2026-03-02 18:22:58 -0400

    feat: Implement navbar component

---
+++ src/app/core/components/navbar/navbar.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+isMenuOpen = signal(false);
+
+  toggleMenu() {
+  this.isMenuOpen.update(v => !v);
+  }
+}
