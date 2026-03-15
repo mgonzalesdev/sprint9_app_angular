9ea83eed5d177e7c8f291398b645f7d47beb8489
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2026-03-15 13:10:33 -0400

    feat: Implement component ProductAdminRow

--- src/app/shared/components/product-admin-row/product-admin-row.ts
+++ src/app/shared/components/product-admin-row/product-admin-row.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Product } from '@core/models/catalog.model';
+import { ProductService } from '@core/services/product';
 
 @Component({
   selector: 'app-product-admin-row',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './product-admin-row.html',
   styleUrl: './product-admin-row.scss',
 })
 export class ProductAdminRow {
+  private productService = inject(ProductService);
+  product = input.required<Product>();
+  onDelete = output<number>();
+
+  onDeleteClick() {
+    // Para acceder al valor de un input signal se usan paréntesis ()
+    this.onDelete.emit(this.product().id);
+  }
 
 }
